93a8efa03c500a7dacc26f16daa8c6796fa17ba8
kevin kalujerovich <kevinkalujerovich@gmail.com>
2021-05-09 01:37:05 -0300

    segunda entrega

---
+++ src/components/navbar.js
@@ -0,0 +1,14 @@
+import {AppBar,Toolbar,IconButton,Typography,Button} from '@material-ui/core/';
+
+const Navbar = () =>{
+    return  <AppBar position="static">
+    <Toolbar>
+      <Typography variant="h6" >Tienda</Typography>
+    </Toolbar>
+  </AppBar>
+    };
+
+
+
+
+export default Navbar;
