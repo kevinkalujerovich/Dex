83226ced5d14176d310d8f6a15ff1e18ba715a65
kevin kalujerovich <kevinkalujerovich@gmail.com>
2021-05-18 02:22:46 -0300

    Componentes II

--- src/components/ItemCount.js
+++ src/components/ItemCount.js
@@ -3,13 +3,31 @@ import {  useState } from "react";
 export default function ItemCount({initial,stock}) {
   const [counter, setCounter] = useState(initial);
   const [error, setError] = useState("");
+  const [errorBis,setErrorBis]=useState("");
 
-  const changeCounter = (value) => {
-    setCounter(counter + value);
+  const restarCounter = () => {
+    return counter==1 ? (
+    setError(`No puede ingresar una cantidad menor a 1`),
+    setErrorBis(``)
+    ):(
+    setCounter(counter-1),
+    setErrorBis(``)
+    );
+  };
+  
+  const sumarCounter = () => {
+    return counter==10?(
+      setError(`No puede sumar mas unidades llego al limite de stock`),
+      setErrorBis(``)
+    ):(
+      setCounter(counter+1),
+      setErrorBis(``)
+    );
   };
 
 const onAdd=()=>{
-  counter<stock?setError(`Agrego al carrito ${counter}`):setError(`El stock actual es ${stock}`);
+setErrorBis(`Agrego al carrito ${counter} productos`);
+setError(``);
 }
 
 
@@ -17,11 +35,12 @@ const onAdd=()=>{
   return (
       <div>
         <h1>CANTIDAD: {counter}</h1>
-        <button onClick={() => changeCounter(-1)}>Restar uno</button>
-        <button onClick={() => changeCounter(1)}>Sumar uno</button>
+        <button onClick={() => restarCounter()}>Restar uno</button>
+        <button onClick={() => sumarCounter()}>Sumar uno</button>
+        <h2>{error}</h2>
         <br/><br/><br/>
         <button onClick={onAdd}>Agregar al carrito</button>
-        <h2>{error}</h2>
+        <h2>{errorBis}</h2>
       </div>
   );
 }
