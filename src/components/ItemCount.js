57316513efb6adda4cf4044be914bb23245b8ba9
kevin kalujerovich <kevinkalujerovich@gmail.com>
2021-06-04 01:57:42 -0300

    desafio count

--- src/components/ItemCount.js
+++ src/components/ItemCount.js
@@ -1,10 +1,12 @@
 import {  useState } from "react";
 import ButtonGroup from '@material-ui/core/ButtonGroup';
 import Button from '@material-ui/core/Button';
-import {Box} from '@material-ui/core';
+import {Box,Grid} from '@material-ui/core';
+import{NavLink} from "react-router-dom";
 
 export default function ItemCount({initial,stock}) {
   const [counter, setCounter] = useState(initial);
+  const [show, setShow] = useState(true);
   
   const restarCounter = () => {
     if(counter>1){
@@ -18,11 +20,13 @@ export default function ItemCount({initial,stock}) {
   };
 
 const onAdd=()=>{
+  setShow(false);
 }
 
 
-
   return (
+    <Grid container spacing={2} >
+    <Grid item xs>
     <Box
   display="flex"
   justifyContent="center"
@@ -33,5 +37,17 @@ const onAdd=()=>{
   <Button onClick={() => sumarCounter()}>+</Button>
 </ButtonGroup>
 </Box>
+    </Grid>
+    <Grid item xs>
+      <>
+      {show===true?
+    <Button variant="contained" color="primary" onClick={() => {onAdd()
+    }}>Agregar al carrito</Button>:<NavLink to="/cart"
+    style={{ textDecoration: 'none',color:"white" }}><Button variant="contained" color="primary">Terminar mi compra</Button></NavLink>}
+    </>
+    </Grid>
+    </Grid>
+   
   );
 }
+
--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import {Typography,Grid,Box,Button} from '@material-ui/core';
-import ItemCount from './ItemCount'
+import {Typography,Grid,Box} from '@material-ui/core';
+import ItemCount from './ItemCount';
 
 
 export default function ItemDetail({img,description,name,precio}) {
@@ -20,17 +20,10 @@ export default function ItemDetail({img,description,name,precio}) {
         {description}
       </Typography>
       <Box mt={6}>
-      <Grid container spacing={2} >
-        <Grid item xs>
       <ItemCount stock={10} initial={1}/>
-        </Grid>
-        <Grid item xs>
-        <Button variant="contained" color="primary" >Agregar al carrito</Button>
-        </Grid>
-        </Grid>
+      
         </Box>
       </Grid>
-     
         </Grid>
   );
 }
