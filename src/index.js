240edabe0a6fe938b7a53aad37128fd3485462b3
Kalu <kevinkalujerovich@gmail.com>
2021-07-08 22:42:51 -0300

    asd

--- src/index.js
+++ src/index.js
@@ -2,7 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { HashRouter } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>,
+
+  document.getElementById("root")
+);
 
 reportWebVitals();
